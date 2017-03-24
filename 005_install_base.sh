e839c058bd291a7dde4c15ce795d657984811a45
Peter Preeper <ppreeper@gmail.com>
2017-03-24 12:15:59 -0600

    add firefox for budgie desktop

--- 005_install_base.sh
+++ 005_install_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt -y install man-db vim-nox mc fdupes wget curl aria2 curlftpfs sshfs nfs-common cpio bzip2 lzop pigz unzip unrar swaks fio htop gddrescue tree nmon
-sudo apt -y install chromium-browser
+sudo apt -y install chromium-browser firefox
 sudo apt -y install python-software-properties pkg-config software-properties-common
 sudo apt -y install build-essential git
 wget -O ttf-mscorefonts-installer_3.6_all.deb http://ftp.ca.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb
