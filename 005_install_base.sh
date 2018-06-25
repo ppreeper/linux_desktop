73cef24ac2be6bfc6e3519c81187aa0540f8d5ef
Peter Preeper <ppreeper@gmail.com>
2018-06-25 12:38:48 -0600

    chmod +x *sh

--- 005_install_base.sh
+++ 005_install_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt -y install man-db vim-nox mc fdupes wget curl aria2 curlftpfs sshfs nfs-common cpio bzip2 lzop pigz unzip unrar swaks fio htop gddrescue tree nmon cabextract
+sudo apt -y install man-db vim-nox mc fdupes wget curl aria2 curlftpfs sshfs nfs-common cpio bzip2 lzop pigz unzip unrar swaks fio htop gddrescue tree nmon cabextract duplicity
 sudo apt -y install chromium-browser firefox
 sudo apt -y install python3-software-properties pkg-config software-properties-common
 sudo apt -y install build-essential git
