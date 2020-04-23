a5d3b231a4e967f6f30a126dadf3d8dbe71a7fc7
Peter Preeper <ppreeper@gmail.com>
2020-04-22 22:07:54 -0600

    add first playbook

--- focal.sh
+++ focal.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 sudo apt install git ansible -y
+
+git clone https://github.com/ppreeper/linux_desktop $HOME/linux_desktop
+
+cd $HOME/linux_desktop
+
+ansible-playbook -b -K focal/00-bin.yml
